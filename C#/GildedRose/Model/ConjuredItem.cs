f7f48b8ff831e54acb7057519dc3356083aa4dbf
argarm <aarongrmrr@gmail.com>
2019-11-07 13:45:56 +0000

    refactor RegularItem Class

--- C#/GildedRose/Model/ConjuredItem.cs
+++ C#/GildedRose/Model/ConjuredItem.cs
@@ -8,10 +8,30 @@ namespace GildedRose.Model
     {
         public override void UpdateProductDetails(Item item)
         {
-            item.SellIn -= 1;
-            if (item.SellIn < 0 && item.Quality > 4) { item.Quality -= 4; return; }
-            if (item.Quality > 1) {item.Quality -= 2; return;}
+            DecreaseSellInOfConjuredItem(item);
+            UpdateQualityOfConjuredItem(item);
+        }
+
+        private static void UpdateQualityOfConjuredItem(Item item)
+        {
+            if (item.SellIn < 0 && item.Quality > 4)
+            {
+                item.Quality -= 4;
+                return;
+            }
+
+            if (item.Quality > 1)
+            {
+                item.Quality -= 2;
+                return;
+            }
+
             item.Quality = 0;
+        }
+
+        private static void DecreaseSellInOfConjuredItem(Item item)
+        {
+            item.SellIn -= 1;
         }
     }
 }
--- C#/GildedRose/Model/RegularItem.cs
+++ C#/GildedRose/Model/RegularItem.cs
@@ -9,9 +9,19 @@ namespace GildedRose
         private static int MINIMUM_QUALITY = 0;
         public override void UpdateProductDetails(Item item)
         {
+            DecreaseSellInRegularItem(item);
+            UpdateQualityOfRegularItem(item);
+        }
+
+        private static void UpdateQualityOfRegularItem(Item item)
+        {
             if (item.Quality > MINIMUM_QUALITY) item.Quality -= 1;
             if (item.SellIn <= 0) item.Quality -= 1;
             if (item.Quality < MINIMUM_QUALITY) item.Quality = MINIMUM_QUALITY;
+        }
+
+        private static void DecreaseSellInRegularItem(Item item)
+        {
             item.SellIn -= 1;
         }
     }
